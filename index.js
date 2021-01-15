55757c2aaf4917bab22ef51a13fdfe37c43c6694
zhanghao05 <cmcesummer@126.com>
2021-01-15 15:23:07 +0800

    更新

--- index.js
+++ index.js
@@ -1,38 +1,32 @@
-const path = require("path");
-const fs = require("fs");
 const finder = require("./finder");
+const mail = require("./mail");
 
 const argv = process.argv.splice(2);
 
-console.log(argv[0]);
+const person = JSON.parse(argv[0]);
+const user = argv[1];
+const pass = argv[2];
 
-console.log(JSON.parse(argv[0]));
+function sleep(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
 
-// function sleep(ms) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve();
-//     }, ms);
-//   });
-// }
-
-// (async () => {
-//   const configString = JSON.parse(fs.readFileSync(path.join(__dirname, "./config.json"), "utf8"));
-//   const keys = Object.keys(configString);
-
-//   for (const key of keys) {
-//     await sleep(1000);
-//     let sendMsg = "";
-//     const fund = configString[key];
-//     for (const id of fund) {
-//       const msg = await finder(id);
-//       sendMsg += msg + `   ============   \n   `;
-//       await sleep(500);
-//     }
-//     // sender(key, "", sendMsg);
-//     console.log(sendMsg);
-//   }
-// })();
-// timer(() => {
-//   console.log(Date.now());
-// });
+(async () => {
+  const keys = Object.keys(person);
+  for (const key of keys) {
+    await sleep(5000);
+    let sendMsg = "";
+    const fund = person[key];
+    for (const id of fund) {
+      const msg = await finder(id);
+      sendMsg += msg + `   ============   \n   `;
+      await sleep(500);
+    }
+    console.log(sendMsg);
+    mail(user, pass, sendMsg, to);
+  }
+})();
---
+++ mail.js
@@ -0,0 +1,24 @@
+var nodemailer = require("nodemailer");
+module.exports = function mail(user, pass, html = "", to = "", title = "mail") {
+  var mailTransport = nodemailer.createTransport({
+    host: "smtp.qq.com",
+    secureConnection: true, // 使用SSL方式（安全方式，防止被窃取信息）
+    auth: {
+      user,
+      pass,
+    },
+  });
+  var options = {
+    from: `"你的" <${user}>`,
+    to,
+    subject: title,
+    text: title,
+    html,
+  };
+
+  mailTransport.sendMail(options, function (err, msg) {
+    if (err) {
+      console.log(err);
+    }
+  });
+};
