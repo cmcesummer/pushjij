3333cbf597f6045d358e98998a91789a422bcc31
zhanghao05 <cmcesummer@126.com>
2021-02-21 15:11:54 +0800

    更新

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const finder = require("./finder");
 const mail = require("./mail");
 const chtml = require("./createHtml");
+const code = require("./code");
 
 const argv = process.argv.splice(2);
 
-const person = JSON.parse(argv[0]);
+const person = argv[0];
+// const person = JSON.parse(argv[0]);
 const user = argv[1];
 const pass = argv[2];
 
@@ -19,12 +21,30 @@ function sleep(ms) {
   });
 }
 
+// (async () => {
+//   const keys = Object.keys(person);
+//   for (const key of keys) {
+//     await sleep(5000);
+//     let sendMsg = "";
+//     const fund = person[key];
+//     for (const id of fund) {
+//       const time1 = Date.now();
+//       const msg = await finder(id);
+//       console.log(`[finder use]`, Date.now() - time1);
+//       sendMsg +=  `<tr>${msg}</tr>`;
+//       await sleep(500);
+//     }
+//     mail(user, pass, chtml(sendMsg), `"${key}" <${key}>`);
+//   }
+// })();
+
 (async () => {
-  const keys = Object.keys(person);
+  const keys = person.split(',');
+  let i = 0;
   for (const key of keys) {
     await sleep(5000);
     let sendMsg = "";
-    const fund = person[key];
+    const fund = code[i];
     for (const id of fund) {
       const time1 = Date.now();
       const msg = await finder(id);
@@ -32,6 +52,7 @@ function sleep(ms) {
       sendMsg +=  `<tr>${msg}</tr>`;
       await sleep(500);
     }
+    i++;
     mail(user, pass, chtml(sendMsg), `"${key}" <${key}>`);
   }
 })();
